27d7ea52aabb0ec7f0b4e989605dddd02c9676d9
Alex Kiselich <alexander.kiselich@gmail.com>
2023-05-20 12:11:27 -0600

    Finished User story 4

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -15,6 +15,21 @@ RSpec.describe "applications/show" do
       expect(page).to have_content("I am cool")
       expect(page).to have_content("Scooby")
       expect(page).to have_content("In Progress")
+
+    end
+    it "shows a section on the page to 'Add a pet to this Application', and then I can add a pet's name and be redirected to the show page with that pet's name " do
+      shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+      pet = Pet.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+      ben = Application.create!(applicant: "Ben", address: "2303 East West Drive, Denver, CO 80205", description: "I have a roof")
+
+      visit "/applications/#{ben.id}"
+      expect(page).to have_content("Status: In Progress")
+      expect(page).to have_content("Add a Pet to this Application")
+
+      fill_in("search", with: "Scooby")
+      click_on "Search"
+      expect(current_path).to eq("/applications/#{ben.id}")
+      expect(page).to have_content(pet.name)
     end
   end
 end
