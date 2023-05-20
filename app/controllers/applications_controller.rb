bc68c2254d12a4671619814287e9007c2ec587ce
Lauren Golden <llgolden@gmail.com>
2023-05-20 15:34:52 -0600

    Complete user story 5

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -23,6 +23,13 @@ class ApplicationsController < ApplicationController
     end
   end
 
+  def update
+    application = Application.find(params[:id])
+    pet = Pet.find(params[:pet_id])
+    application.pets << pet
+    redirect_to "/applications/#{application.id}"
+  end
+
   private
 
   def app_params
