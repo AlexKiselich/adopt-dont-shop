4c4054f453dfe885f52b8253518e53475108795b
Lauren Golden <llgolden@gmail.com>
2023-05-21 14:37:07 -0600

    Complete US10

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Application, type: :model do
     it { should have_many :application_pets }
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:applicant) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:description) }
+  end
 end
