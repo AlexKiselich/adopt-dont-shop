c82299876ad9cd366227e18f0351d4fd77e65290
Lauren Golden <llgolden@gmail.com>
2023-05-20 21:41:10 -0600

    Complete US9

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -49,6 +49,7 @@ RSpec.describe "applications/show" do
       expect(current_path).to eq("/applications/#{ben.id}")
       expect(page).to have_content("Pets on this Application: Scooby")
     end
+
 # 6. Submit an Application
     it "allows me to submit an application and changes the status to pending" do
       shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
@@ -75,6 +76,7 @@ RSpec.describe "applications/show" do
       expect(page).to have_content("Pets on this Application: Scrappy")
       expect(page).to_not have_content("Add a Pet to this Application")
     end
+
 # 7. No Pets on an Application
     it "will not allow me to submit an application if there are no pets on the application" do 
       shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
@@ -86,6 +88,27 @@ RSpec.describe "applications/show" do
       expect(page).to_not have_content("Pets on this Application")
       expect(page).to_not have_content("Submit Application")
       expect(page).to_not have_content("Why I would make a good owner for these pet(s)")
+    end
+
+# 9. Case Insensitive Matches for Pet Names
+    it "returns case insensitive matches to a pet search" do 
+      shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+      pet = Pet.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+      pet2 = Pet.create!(name: "Scrappy", age: 1, breed: "Poodle", adoptable: true, shelter_id: shelter.id)
+      pet3 = Pet.create!(name: "SCOOBY SNACKS", age: 3, breed: "Golden Retriever", adoptable: true, shelter_id: shelter.id)
+      pet4 = Pet.create!(name: "scooby-a-looby-a-ding-dong", age: 4, breed: "Labrador Retriever", adoptable: true, shelter_id: shelter.id)
+      pet5 = Pet.create!(name: "hErE wE gO sCoObY-LoO", age: 5, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+      ben = Application.create!(applicant: "Ben", address: "2303 East West Drive, Denver, CO 80205", description: "I have a roof")
+
+      visit "/applications/#{ben.id}"
+      fill_in("search", with: "Scooby")
+      click_on "Search"
+
+      expect(page).to have_content(pet.name)
+      expect(page).to have_content(pet3.name)
+      expect(page).to have_content(pet4.name)
+      expect(page).to have_content(pet5.name)
+      expect(page).to_not have_content(pet2.name)
     end
   end
 end
