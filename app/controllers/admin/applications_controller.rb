831a8de5a7052e5410c74788ee61e47e26346989
Lauren Golden <llgolden@gmail.com>
2023-05-22 22:44:04 -0600

    Refactoring

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,16 +1,12 @@
 class Admin::ApplicationsController < ApplicationController
 
   def show 
-    # require 'pry'; binding.pry
     @application = Application.find(params[:id])
     @pets = @application.pets
   end
 
   def update
-    # ApplicationPet.create(application_id: application.id, pet_id: pet.id)
-    # @button = params[:button]
     application = Application.find(params[:id])
-    # require 'pry'; binding.pry
     application.update(status: "Approved")
     application.save 
     redirect_to "/admin/applications/#{application.id}"
