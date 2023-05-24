5d87ec160b930771fe3fa5280bb8746ff028e5ea
Alex Kiselich <alexander.kiselich@gmail.com>
2023-05-24 15:18:30 -0600

    Refactor part 2

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -4,15 +4,7 @@ class Admin::ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets = @application.pets
     @pet_applications = @application.application_pets
-    # @app_id = ApplicationPet.find_by(application_id: @application.id, pet_id:
-    # find by both FK
-    #     if ApplicationPet.all.first.application_id = @application.id
-    # require 'pry'; binding.pry
-    # @ap_id = ApplicationPet.all.first
-    # @application_pet = ApplicationPet.find(@ap_id)
   end
-
-
 
   def update
     if params[:reject] != nil
@@ -30,26 +22,5 @@ class Admin::ApplicationsController < ApplicationController
     redirect_to "/admin/applications/#{application.id}"
   end
 
-  # def reject 
-  #   application = Application.find(params[:id])
-  #   application.update(status: "Rejected")
-  #   application.save 
-  #   redirect_to "/admin/applications/#{application.id}"
-  # end
 end
 
-
-  # <% @pets.each do |pet|%>
-  #   <div id="app_status-<%= pet.id %>">
-  #   <p>Pets on this Application: <%= link_to pet.name, "/pets/#{pet.id}" %> 
-  #   <% if @pet_applications.ap_status == ("Pending") %> 
-  #     <p><%= button_to "Approve", action: "update", :accept => @pet_applications.id %></p>
-  #     <p><%= button_to "Reject", action: "update", :reject => @pet_applications.id %></p>
-  #   <% elsif @pet_applications.ap_status == ("Rejected") %>
-  #     <p>Pet Application Status: Rejected</p>
-  #   <% else %>
-  #     <p>Pet Application Status: Approved</p>
-  #   </div>
-  #   <% end %>
-  # <% end %>
-  # <p>Application Status: <%= @application.status %></p
